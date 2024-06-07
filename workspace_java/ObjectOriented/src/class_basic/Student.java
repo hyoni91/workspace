3db8b148326c7999c4f4e853edbd341bd62168d9
hyoni91 <hyoni.green@gmail.com>
2024-06-07 17:25:58 +0900

    aa

--- workspace_java/ObjectOriented/src/class_basic/Student.java
+++ workspace_java/ObjectOriented/src/class_basic/Student.java
@@ -6,9 +6,19 @@ public class Student {
     int score;
     // 밑에 선언해도 오류나지 않음! 하지만, 통상적으로 변수 먼저 선언함
 
+
+
+    //데이터관리, 재사용성, 안전성을 위해 메서드로 값 입력 받기.
     //매개변수로 전달된 문자열을 name 값을 변경
     public void setName(String reName){
         name = reName;
+    }
+
+    //나이를 변경하는 메서드
+    public void setAge(int reAge){
+        age = reAge;
+        //0~200까지 제한 등등을 걸 수 있음
+
     }
 
     public void stuIntro(){
--- workspace_java/ObjectOriented/src/class_basic/StudentTest.java
+++ workspace_java/ObjectOriented/src/class_basic/StudentTest.java
@@ -1,5 +1,7 @@
 package class_basic;
 
+//클래스의 필드에 직접 접근하는 것보다는 메서드를 통해 값을 입력하는 것이 좋은 프로그래밍 관행.
+
 public class StudentTest {
     public static void main(String[] args) {
         //Student객체 stu1생성
@@ -14,17 +16,19 @@ public class StudentTest {
 
         System.out.println("---------------------");
 
-        //stu1 값 넣고 출력
+        //stu1 값 넣고 출력 *비추! 프로그램의 유지보수성과 재사용성을 높일 수 있다.
         stu1.name = "김석진";
         stu1.age = 30;
         stu1.score = 100;
         stu1.stuIntro();
 
         System.out.println("---------------------");
-
+        //객체명.메서드명 호출로 값 변경
         stu1.setName("타타");
         stu1.stuIntro();  // name 변경
 
-
+        System.out.println("---------------------");
+        stu1.setAge(26); // age 변경
+        stu1.stuIntro();
     }
 }
